6f0667fc058f76733439cfa2867ad6781295b3c0
makhnoonmk07 <makhnoonmahirakabir@gmail.com>
2026-03-24 23:21:18 +0600

    Add files via upload

---
+++ JavaintroC251227.java
@@ -0,0 +1,13 @@
+
+package javaintro.c251227;
+
+
+public class JavaintroC251227 {
+
+    
+    public static void main(String[] args) {
+         String name = "Mahira";
+    System.out.println("Hello " + name);
+    }
+    
+}
