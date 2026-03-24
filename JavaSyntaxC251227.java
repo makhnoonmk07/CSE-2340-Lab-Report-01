df1b0b1677d63028eadc0a33f7d6488943ef80ad
makhnoonmk07 <makhnoonmahirakabir@gmail.com>
2026-03-24 23:26:46 +0600

    Add files via upload

---
+++ JavaSyntaxC251227.java
@@ -0,0 +1,12 @@
+
+package javasyntax.c251227;
+
+
+public class JavaSyntaxC251227 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
