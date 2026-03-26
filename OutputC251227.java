43afef77faa0c26fd30103dc768150f2c79a632a
makhnoonmk07 <makhnoonmahirakabir@gmail.com>
2026-03-26 23:40:46 +0600

    Add files via upload

---
+++ OutputC251227.java
@@ -0,0 +1,14 @@
+
+package output.c251227;
+
+
+public class OutputC251227 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+    }
+    
+}
