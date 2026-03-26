d926cbdef807f72c1c743ab331747540fda54b89
makhnoonmk07 <makhnoonmahirakabir@gmail.com>
2026-03-26 23:43:39 +0600

    Add files via upload

---
+++ Java_CommentsC251227.java
@@ -0,0 +1,15 @@
+
+package java_comments.c251227;
+
+public class Java_CommentsC251227 {
+    
+    public static void main(String[] args) {
+        //This a single line comment
+        System.out.println("Single line comment");
+        
+        /* The code below will print the words multiline comment
+         to the screen, and it is amazing */
+       System.out.println("Multi line comment");
+    }
+    
+}
---
+++ Java_VariablesC251227.java
@@ -0,0 +1,17 @@
+
+package java_variables.c251227;
+
+
+public class Java_VariablesC251227 {
+
+  
+    public static void main(String[] args) {
+      String name = "John";
+      System.out.println(name);
+      
+      int myNum = 15;
+      System.out.println(myNum);
+  
+    }
+    
+}
